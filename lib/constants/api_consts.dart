d9fb381e64439b9db43d881f4df7fc65aa0cd766
Mohammed Asif <82872316+asifmohammed3@users.noreply.github.com>
2023-02-26 17:38:47 +0530

    second

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,3 +1,3 @@
 String BASE_URL = "https://api.openai.com/v1";
 
-String API_KEY = "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";
+String API_KEY = "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";
